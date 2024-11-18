370ec4cc4cdf5044c4eab63e716c34395253c27b
v0ndage <anywallsocket@gmail.com>
2024-11-18 13:19:48 -0500

    Update runDFTs.sh

--- RUN/runDFTs.sh
+++ RUN/runDFTs.sh
@@ -7,7 +7,6 @@
 #SBATCH --output=%J.out
 #SBATCH --job-name=DFT
 
-
 START=$1
 STOP=$2
 
@@ -17,11 +16,8 @@ for ((i=$START; i<=$STOP; i++));
 do
     #make and enter n DFT dirs
     if cd "$i"; then
-        
-	cp -r ../../run.sh .
+	#cp -r ../../run.sh .
     	sed -i "/--job-name/,//s/NN/DFT-$i/" run.sh
-    
-    	#run dfts
     	#sbatch run.sh
     	sbatch --partition=preemptable --qos=preemptable run.sh
     	sleep 2
