ad61fcd6691c1b92eae2efa631256584ad40066a
v0ndage <anywallsocket@gmail.com>
2025-01-29 10:53:33 -0500

    Update rerun.sh

--- RUN/rerun.sh
+++ RUN/rerun.sh
@@ -21,60 +21,64 @@ mkdir SAVE$C
 
 for ((i=$START; i<=$STOP; i++));
 do
-	cd $i/
+	# enter only if dir exists
+	if [[ -d "$i" ]]; then
 
-	# check job
-	if [[ -f "OUTCAR" ]]; then
-		
-		# at least one iteration (otherwise presumed stuck)
-		if grep -q "LOOP" "OUTCAR"; then
-			
-			# check completion
-			if grep -q "required" "OUTCAR"; then
-				echo "$i done"
-				cd ..
-				continue
-				
-			# check error
-			elif grep -q "ZBRENT" "OUTCAR"; then
-				echo "$i error"
-				#improve resolution
-				sed -i 's/^EDIFF *=.*/EDIFF = 5E-4/' "INCAR"
-				
-			else
-				echo "$i undone"
-			fi
-			
-		else
-			# find and cancel jobs that are presumed stuck
-			for file in *.err; do
-				if [[ -f "$file" ]]; then
-					jobID=$(basename "$file" .err)
-					echo "$i canceled"
-					scancel "$jobID"
-					rm OUTCAR
+		cd $i/
+
+		# check job
+		if [[ -f "OUTCAR" ]]; then
+
+			# at least one iteration (otherwise presumed stuck)
+			if grep -q "LOOP" "OUTCAR"; then
+
+				# check completion
+				if grep -q "required" "OUTCAR"; then
+					echo "$i done"
+					cd ..
+					continue
+
+				# check error
+				elif grep -q "ZBRENT" "OUTCAR"; then
+					echo "$i error"
+					#improve resolution
+					sed -i 's/^EDIFF *=.*/EDIFF = 5E-4/' "INCAR"
+
+				else
+					echo "$i undone"
 				fi
-			done
+
+			else
+				# find and cancel jobs that are presumed stuck
+				for file in *.err; do
+					if [[ -f "$file" ]]; then
+						jobID=$(basename "$file" .err)
+						echo "$i canceled"
+						scancel "$jobID"
+						rm OUTCAR
+					fi
+				done
+			fi
+
+			# rerun job
+			mv "POSCAR" "POS1"
+			mv "OUTCAR" "OUT1"
+			mv "CONTCAR" "POSCAR"
+			sed -i 's/^ISTART *=.*/ISTART = 1/' "INCAR"
+			sed -i 's/^ICHARG *=.*/ICHARG = 0/' "INCAR"
+
+			echo "running $i"
+			#sbatch run.sh
+			sbatch --partition=preemptable --qos=preemptable run.sh
+			sleep 2
+
+		else
+			echo "$i no OUTCAR"
 		fi
-		
-		# rerun job
-		mv "POSCAR" "POS1"
-		mv "OUTCAR" "OUT1"
-		mv "CONTCAR" "POSCAR"
-		sed -i 's/^ISTART *=.*/ISTART = 1/' "INCAR"
-		sed -i 's/^ICHARG *=.*/ICHARG = 0/' "INCAR"
-		
-		echo "running $i"
-		#sbatch run.sh
-		sbatch --partition=preemptable --qos=preemptable run.sh
-		sleep 2
 
-	else
-		echo "$i no OUTCAR"
+		cd ..
 	fi
-	
-	cd ..
-
 done
 cd ../
+
 ###
